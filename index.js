5b3531f0ed74b5e898a0ee78720b470a7855538a
samk <samuelkariuki1@student.moringaschool.com>
2023-06-14 22:44:53 +0300

    Done with string labs

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !` ;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+shortGreeting;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+"Edsger Dijkstra"[0]
+"Edsger Dijkstra".charAt(0)
+"Edsger Dijkstra".length;
+"Edsger Dijkstra"[15];
+"Edsger Dijsktra"[14]
+"Edsger Dijsktra".slice();
+"Edsger Dijsktra".slice(7);
+"Edsger Dijsktra".slice(0,3);
+currentUser.slice(0,1);
+
+
+
+
